805ea74fd8596b26c0f8c8ad59c25f0a507c2390
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-03-11 17:18:10 +0100

    TTK-24934 remove api import user on multimedia objects

--- Controller/ImportController.php
+++ Controller/ImportController.php
@@ -26,7 +26,7 @@ class ImportController extends Controller
         }
 
         $opencastImportService = $this->get('pumukit_opencast.import');
-        $opencastImportService->importRecordingFromMediaPackage($mediaPackage['mediapackage'], false, $this->getUser());
+        $opencastImportService->importRecordingFromMediaPackage($mediaPackage['mediapackage'], false, null);
 
         return new Response('Success', Response::HTTP_OK);
     }
